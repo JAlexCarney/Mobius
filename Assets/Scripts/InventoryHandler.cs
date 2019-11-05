194251d025ee03f92e99488b8b8891aa6b2a6884
J. Alex Carney <38840263+JAlexCarney@users.noreply.github.com>
2019-11-04 16:48:12 -0800

    Initial commit

---
+++ Assets/Scripts/InventoryHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryHandler : MonoBehaviour
+{
+    private int numItems = 0;
+
+    public void Colect(string name)
+    {
+        Debug.Log(name);
+        GameObject thing = GameObject.Find(name);
+        numItems++;
+        GameObject slot = GameObject.Find("Slot" + numItems);
+        GameObject item = slot.transform.Find("Item").gameObject;
+        item.GetComponent<Image>().sprite = thing.GetComponent<Image>().sprite;
+        item.GetComponent<Image>().color = new Color(255, 255, 255, 255);
+        Destroy(thing);
+    }
+}
---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public void PlayerOneStart()
+    {
+        LoadRoom(1, 1);
+    }
+
+    public void PlayerTwoStart()
+    {
+        LoadRoom(2, 1);
+    }
+
+    public void LoadRoom(int player, int room)
+    {
+        string sceneName = "Player" + player + "Room" + room;
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void StartMenu()
+    {
+        SceneManager.LoadScene("StartMenu", LoadSceneMode.Single);
+    }
+
+}
