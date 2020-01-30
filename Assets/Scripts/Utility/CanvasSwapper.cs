278a1a41eaf20a4f3c620d05a19c52d3b280f991
J Alex Carney <JAlexCarney@gmail.com>
2020-01-29 22:42:06 -0800

    Made objects Swappable
    - added "Swappable" script
    - added sounds
    - added scene fade

--- Assets/Scripts/Utility/CanvasSwapper.cs
+++ Assets/Scripts/Utility/CanvasSwapper.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasSwapper : MonoBehaviour
 {
@@ -8,6 +9,46 @@ public class CanvasSwapper : MonoBehaviour
     public GameObject prevCanvas;
     public GameObject inventory;
     public GameObject map;
+    public GameObject fade;
+
+    private Image fadeImage;
+    private void Start()
+    {
+        fadeImage = fade.GetComponent<Image>();
+    }
+
+    private readonly int delay = 15;
+    private int count = 0;
+    private bool fadingOut = false;
+    private bool fadingIn = false;
+    private string goingTo = "";
+    void Update()
+    {
+        if (fadingOut)
+        {
+            count++;
+            fadeImage.color = new Vector4(0.15f, 0.15f, 0.15f, (float)count / delay);
+            if (count == delay)
+            {
+                fadingOut = false;
+                fadingIn = true;
+                SwitchCanvasMaintainUI(goingTo);
+                count = 0;
+            }
+        }
+        else if (fadingIn)
+        {
+            fadeImage.color = new Vector4(0.15f, 0.15f, 0.15f, 1f - ((float)count / delay));
+            count++;
+            if (count == delay)
+            {
+                fadingIn = false;
+                goingTo = "";
+                count = 0;
+                fade.SetActive(false);
+            }
+        }
+    }
 
     public void SwitchCanvasNoUI(string newCanvas)
     {
@@ -31,9 +72,16 @@ public class CanvasSwapper : MonoBehaviour
         currentCanvas = canvasToActivate;
     }
 
+    public void SwitchCanvasMaintainUIAndFade(string newCanvas)
+    {
+        fadingOut = true;
+        goingTo = newCanvas;
+        fade.SetActive(true);
+    }
+
     public void ReturnToPreviousCanvas()
     {
-      string backUp = prevCanvas.transform.name;
-      SwitchCanvasMaintainUI(backUp);
+        string backUp = prevCanvas.transform.name;
+        SwitchCanvasMaintainUI(backUp);
     }
 }
