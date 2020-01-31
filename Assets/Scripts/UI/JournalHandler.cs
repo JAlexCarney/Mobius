1f3fa226ce54d2321770954f10785386bd52d109
Paula <pnavarr1@ucsc.edu>
2020-01-30 18:58:13 -0800

    Updated Start() of JournalHandler to use a new method to find LeftText and RightText, since Unity Dumb and can't find inactive children.

--- Assets/Scripts/UI/JournalHandler.cs
+++ Assets/Scripts/UI/JournalHandler.cs
@@ -1,31 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JournalHandler : MonoBehaviour
 {
     //List<List<string>>
     public GameObject IndicatorPrefab;
-    private GameObject leftText;
-    private GameObject rightText;
+    private Text leftText;
+    private Text rightText;
     private List<string> hints;
 
     private void Start()
     {
-        leftText = GameObject.Find("LeftText");
-        rightText = GameObject.Find("RightText");
+        //get text children of the journal canvas
+        //unity can't find inactive children using Find() :(
+        Text[] ts = gameObject.GetComponentsInChildren<Text>(true);
+        foreach (Text t in ts)
+        {
+            Debug.Log(t.gameObject.name);
+            string name = t.gameObject.name; 
+            if (t != null && name.Equals("LeftText"))
+                leftText = t;
+            else if (t != null && name.Equals("RightText"))
+                rightText = t; 
+        }
+
+        Debug.Log(leftText);
+        Debug.Log(rightText);
+
         hints = new List<string>();
     }
 
     // fill up that dang list list
-    void AddEntry(string entry)
+    public void AddEntry(string entry)
     {
         hints.Add(entry);
+        Debug.Log(leftText);
+        Text textComp = leftText.GetComponent<Text>();
+        
+        TextGenerator t = textComp.cachedTextGenerator;
+        string result = textComp.text.Substring(0, t.characterCountVisible);
+        Debug.Log("Generated " + t.characterCountVisible + " characters");
+        Debug.Log("Visible string is: ");
+        Debug.Log(result);
+
     }
 
     void Display()
     {
-
+        Debug.Log("hahahahhaaha");
+        //Text textComp = GetComponent<Text>();
+        //TextGenerator t = textComp.cachedTextGenerator;
+        //string result = textComp.text.Substring(0, t.characterCountVisible);
+        //Debug.Log("Generated " + t.characterCountVisible + " characters");
+        //Debug.Log("Visible string is: ");
+        //Debug.Log(result);
+        // B A S I C A L L Y add entry 
+        //then check if left text all filled up
+        //if not, add /n + entry 
+        //else, add to right
+        //refresh?? idek 
     }
 
     // display indicator that an entry has been placed
