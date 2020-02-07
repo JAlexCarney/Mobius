8809f08bbff4993f3d0f9f3eb83ecd89e42d70fd
J. Alex Carney <38840263+JAlexCarney@users.noreply.github.com>
2020-02-06 17:26:19 -0800

    Vending Machine Puzzle
     - Added stub of vending machine puzzle

---
+++ Assets/Scripts/Puzzle/Arrangable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrangable : MonoBehaviour
+{
+    public Vector2Int correctPos;
+    public Vector2Int currentPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Puzzle/Arrangement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrangement : MonoBehaviour
+{
+    public int width;
+    public int height;
+    private Arrangable[][] arrangables;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrangables = new Arrangable[width][];
+        for (int i = 0; i < width; i++)
+        {
+            arrragables = 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Check()
+    {
+
+    }
+
+    void Scramble()
+    {
+
+    }
+}
