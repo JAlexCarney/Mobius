a9a79d7f03199152d580b914e468eb47e0a30f4f
Paula <pnavarr1@ucsc.edu>
2020-02-13 22:09:24 -0800

    Journal reverted to text version (Sorry Alex :[ ). Also did some sort of needless refractoring of stuff to make the variable/method names more accurate.

--- Assets/Scripts/UI/JournalHandler.cs
+++ Assets/Scripts/UI/JournalHandler.cs
@@ -7,82 +7,51 @@ public class JournalHandler : MonoBehaviour
 {
     //List<List<string>>
     public GameObject IndicatorPrefab;
-    //private Text leftText;
-    //private Text rightText;
+    private Text leftPage;
+    private Text rightPage;
     private List<string> hints;
-
-    public GameObject journalNodePrefab = null;  
-    private GameObject[][] JournalNodes = null;
 
     private void Start()
     {
-        // get parent folder for node objects
-        GameObject nodes = transform.Find("Nodes").gameObject;
-
-        // initialize nodes array
-        JournalNodes = new GameObject[6][];
-        for (int i = 0; i < JournalNodes.Length; i++)
+        //get text children of the journal canvas
+        //unity can't find inactive children using Find() :(
+        Text[] ts = gameObject.GetComponentsInChildren<Text>(true);
+        foreach (Text t in ts)
         {
-            JournalNodes[i] = new GameObject[7];
-        }
-
-        // initialize constants
-        float nodeWidth = nodes.GetComponent<RectTransform>().sizeDelta.x/ JournalNodes.Length;
-        float nodeHeight = nodes.GetComponent<RectTransform>().sizeDelta.y/ JournalNodes[0].Length;
-
-        // reset x to left of canvas
-        float x =  0;
-        
-        // create journal entry nodes
-        for (int i = 0; i < JournalNodes.Length; i++)
-        {
-            // reset y value to top of canvas
-            float y = 0;
-            for (int j = 0; j < JournalNodes[i].Length; j++)
+            string name = t.gameObject.name;
+            if (t != null && name.Equals("LeftPage"))
             {
-                // create a temporary prefab node
-                GameObject tmp = Instantiate(journalNodePrefab);
-
-                // Get the rectTransform object
-                RectTransform rectT = tmp.GetComponent<RectTransform>();
-
-                // set the parent canvas of the node
-                tmp.transform.parent = nodes.transform;
-
-                // Set the created nodes position
-                rectT.anchoredPosition = new Vector2(x, y);
-
-                // return to original scale
-                rectT.localScale = new Vector3(1f, 1f, 1f);
-
-                // make clear
-                tmp.GetComponent<Image>().color = Color.clear;
-
-                // Set the size of the nodes
-                rectT.sizeDelta = new Vector2(nodeWidth, nodeHeight);
-                
-                JournalNodes[i][j] = tmp;
-                y -= nodeHeight;
+                leftPage = t;
             }
-            x += nodeWidth;
+            else if (t != null && name.Equals("RightPage"))
+            {
+                rightPage = t;
+            }
         }
-        /*
-        GameObject tmp = Instantiate(journalNodePrefab);
-        tmp.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
-        tmp.GetComponent<RectTransform>().sizeDelta = new Vector2(nodeWidth, nodeHeight);
-        tmp.transform.parent = transform;
-        */
+        hints = new List<string>();
     }
 
-    // fill up that dang list list
+    // fill up that dang list list & display indicator
     public void AddEntry(string entry)
     {
-        
+        if (!hints.Contains(entry))
+        {
+            hints.Add(entry);
+            IndicateEntry();
+            Display();
+        }
+   
     }
 
     void Display()
     {
-        
+        int hintIndex = 0;
+
+        //add hints to leftText
+        hintIndex = AddHintsToTextComponent(leftPage, hintIndex);
+
+        //add leftover hints to rightText
+        AddHintsToTextComponent(rightPage, hintIndex);
     }
 
     /// <summary>
@@ -94,14 +63,14 @@ public class JournalHandler : MonoBehaviour
     /// Returns hints.length + 1 if the end of the hints list is reached.</returns>
     int AddHintsToTextComponent(Text textComponent, int hintIndex)
     {
-        textComponent.text = ""; 
+        textComponent.text = "";
 
         for (; hintIndex < hints.Count; hintIndex++)
         {
             string hint = hints[hintIndex];
 
             //add hint to text
-            textComponent.text += " - " + hint;
+            textComponent.text += hint;
 
             //Update everything so things don't break below lol
             Canvas.ForceUpdateCanvases();
@@ -121,21 +90,17 @@ public class JournalHandler : MonoBehaviour
             textComponent.text += "\n";
 
         }
-        return hintIndex; 
+        return hintIndex;
     }
 
     // display indicator that an entry has been placed
-    public void IndicateEntry(string entry)
+    public void IndicateEntry()
     {
         // Instantiate(prefab)
-        if (!hints.Contains(entry))
-        {
-            GameObject indicator = Instantiate(IndicatorPrefab);
-            Animation anim = indicator.GetComponent<Animation>();
-            anim.Play();
-            Destroy(indicator, anim.GetClip("JournalEntry").averageDuration);
-            AddEntry(entry);
-        }
+        GameObject indicator = Instantiate(IndicatorPrefab);
+        Animation anim = indicator.GetComponent<Animation>();
+        anim.Play();
+        Destroy(indicator, anim.GetClip("JournalEntry").averageDuration);
     }
 
     // make journal invisible
