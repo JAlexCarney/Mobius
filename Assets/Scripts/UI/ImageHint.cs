ec8ad3a2c544555777988a1ed0162db92ef13fee
Paula <pnavarr1@ucsc.edu>
2020-02-14 17:58:24 -0800

    Almond is now draggable within bounds, ft. me crying over UI and rectangles

--- Assets/Scripts/UI/ImageHint.cs
+++ Assets/Scripts/UI/ImageHint.cs
@@ -16,6 +16,8 @@ public class ImageHint : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     static public string held = "";
     static public GameObject heldObj = null;
 
+    private bool colliding;
+
     private Transform parent;
 
     private Touch touch;
@@ -34,11 +36,39 @@ public class ImageHint : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         {
             touch = Input.GetTouch(0);
         }
-        if (isHeld)
+        if (isHeld && !colliding)
         {
             if (Input.touchCount == 0)
             {
-                transform.position = Input.mousePosition;
+                transform.position = new Vector2(Clamp(Input.mousePosition.x, 340, 588),
+                    Clamp(Input.mousePosition.y, 52, 421));
+
+                //code graveyard below, enter at ur own risk ooo00OOOOoooooooOOoo00000ooooo
+
+                //    Input.mousePosition;
+
+                //Vector2 pageSize = parent.GetComponent<RectTransform>().sizeDelta;
+                //float width = pageSize.x;
+                //float height = pageSize.y;
+
+                //float leftBound = parent.position.x;
+                //if (transform.position.x < -(width/2))
+                //    transform.position = new Vector2(-width/2, transform.position.y);
+
+                //float upperBound = parent.position.y + (height / 2); 
+                //if (transform.position.y > ( parent.position.y + height/2))
+                //{
+                //    transform.position = new Vector2(transform.position.x, parent.position.y);
+                //    Debug.Log("AAAAAAAAAAAAAAAAAAA");
+                //}
+                //Debug.Log(-width/2 + " JDFIOEJFIOE " + parent.position.y+" " + height/2 + " almond: " + transform.position.x + "," + transform.position.y);
+
+                //float rightBounds = parent.position.x + parent.GetComponent<RectTransform>().sizeDelta.x;
+                //if (transform.position.x > rightBounds)
+                //    transform.position = new Vector2(rightBounds, transform.position.y);
+                //float lowerBounds = parent.position.y - parent.GetComponent<RectTransform>().sizeDelta.y; 
+                //if (transform.position.y < lowerBounds)
+                //    transform.position = new Vector2(transform.position.x, lowerBounds);
             }
             else
             {
@@ -80,4 +110,11 @@ public class ImageHint : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         heldObj = null;
         justReleased = false;
     }
+
+    //ty https://stackoverflow.com/questions/3176602/how-to-force-a-number-to-be-in-a-range-in-c/3176617
+    public static float Clamp(float value, float min, float max)
+    {
+        return (value < min) ? min : (value > max) ? max : value;
+    }
+
 }
