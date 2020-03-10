ec4e08da91a3740efc11ab241bebf3014390a613
Casey Fields <superpie314@gmail.com>
2020-03-09 18:06:40 -0700

    Vending Machine Revamp
    - Started Implementing Revamped Vending Machine
    - Buttons and GameObjects in
    - Code Enterer and Checker Script Started

---
+++ Assets/Scripts/Puzzle/VendingCodeEnterer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VendingCodeEnterer : MonoBehaviour
+{
+
+    public string[] stateOrder;
+
+    public string displayCode;
+
+    public GameObject display;
+    public GameObject displayVal;
+
+    public GameObject[] indicatorLetters;
+    public GameObject[] indicatorNumbers;
+
+
+    public void displayUpdate(string value)
+    {
+        // Updates display code.
+        displayCode += value;
+        Text displayText = displayVal.GetComponent<Text>();
+        displayText.text = displayCode; 
+
+    }
+
+    bool checkSolution(string solution)
+    {
+        return false;
+    }
+
+    void colorReset(bool solution)
+    {
+        // Change color based on solution correctness, then invoke wipe string.
+
+    }
+
+    void wipeString()
+    {
+        displayCode = string.Empty;
+    }
+}
