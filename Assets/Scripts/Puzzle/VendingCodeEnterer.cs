69ee94faa81f2c9a6e07a3e25debb7830becd7f3
Casey Fields <superpie314@gmail.com>
2020-03-16 20:53:02 -0700

    Vending Machine Updates
    - All assets in
    - Scripts modifications
    - Added event system to resolve the button bug

--- Assets/Scripts/Puzzle/VendingCodeEnterer.cs
+++ Assets/Scripts/Puzzle/VendingCodeEnterer.cs
@@ -6,39 +6,103 @@ using UnityEngine.UI;
 public class VendingCodeEnterer : MonoBehaviour
 {
 
-    public string[] stateOrder;
+    public string[] codestoCheck;
+    public string[] codestoDisplay;
 
     public string displayCode;
+    public int displayCountDebug;
 
     public GameObject display;
-    public GameObject displayVal;
+
+    public Sprite displayTrue;
+    public Sprite displayNeutral;
+    public Sprite displayFalse;
+
+    public Sprite lightOn;
+    public Sprite lightOff;
 
     public GameObject[] indicatorLetters;
     public GameObject[] indicatorNumbers;
 
-
-    public void displayUpdate(string value)
+    private void Start()
     {
+        SetState(codestoDisplay[0]);
+    }
+
+    public void SetState(string state)
+    {
+        // Makes sure no other lights are turned on before activating new lights.
+        foreach(GameObject indicator in indicatorLetters)
+        {
+            indicator.GetComponent<Image>().sprite = lightOff;
+        }
+        foreach (GameObject indicator in indicatorNumbers)
+        {
+            indicator.GetComponent<Image>().sprite = lightOff;
+        }
+
+        // Finds the lights corresponding to the proper code and turns them on.
+        string letter = state[0].ToString();
+        string number = state[1].ToString();
+
+        GameObject letterIndicator = transform.Find("Indicators").Find("Indicator"+letter).gameObject;
+        GameObject numberIndicator = transform.Find("Indicators").Find("Indicator"+number).gameObject;
+
+        letterIndicator.GetComponent<Image>().sprite = lightOn;
+        numberIndicator.GetComponent<Image>().sprite = lightOn;
+    }
+
+    public void DisplayUpdate(string value)
+    {
+        print("Display Update: " + value);
+
         // Updates display code.
         displayCode += value;
-        Text displayText = displayVal.GetComponent<Text>();
-        displayText.text = displayCode; 
+        display.GetComponentInChildren<Text>().text = displayCode;
+
+        displayCountDebug = displayCode.Length;
+
+        // If a complete code is input, checks if the solution is correct.
+        if(displayCode.Length == 2)
+        {
+            colorReset(CheckSolution(displayCode));
+
+            Invoke("DisplayReset", 1);
+        }
 
     }
 
-    bool checkSolution(string solution)
+    bool CheckSolution(string solution)
     {
+        print("Check Solution: " + solution);
+
+        
         return false;
     }
 
     void colorReset(bool solution)
     {
+        print("Setting Color: " + solution);
+
         // Change color based on solution correctness, then invoke wipe string.
+        if(solution)
+        {
+            display.GetComponent<Image>().sprite = displayTrue;
+        }
+        else
+        {
+            display.GetComponent<Image>().sprite = displayFalse;
+        }
 
     }
 
-    void wipeString()
+    void DisplayReset()
     {
+        print("Resetting Display");
+
+        // Reset displayCode and reset display to gray.
         displayCode = string.Empty;
+        display.GetComponentInChildren<Text>().text = displayCode;
+        display.GetComponent<Image>().sprite = displayNeutral;
     }
 }
