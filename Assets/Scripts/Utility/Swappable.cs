b3ef0e1c54e9ead128799b6b97b09ed3a167f58e
Paula <pteriwinkle@gmail.com>
2020-04-08 16:08:11 -0700

    debug message printed when something is swappable on density puzzle

--- Assets/Scripts/Utility/Swappable.cs
+++ Assets/Scripts/Utility/Swappable.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,21 +15,20 @@ public class Swappable : MonoBehaviour
 
     public void Update()
     {
-        // Check if the left mouse button was raised
-        if (Draggable.justReleased)
-        {
+        
             if (Draggable.holding)
             {
                 // Check if the mouse was clicked over a UI element
                 if (CheckBounds(Draggable.heldObj) && Draggable.heldObj != gameObject && Draggable.heldObj.GetComponent<Swappable>())
                 {
-                    Draggable.justReleased = false;
-                
+
                     Draggable heldDrag = Draggable.heldObj.GetComponent<Draggable>();
                     Draggable thisDrag = GetComponent<Draggable>();
 
-                    if (Draggable.heldObj.GetComponent<Swappable>().swapID == swapID)
+                    if (Draggable.heldObj.GetComponent<Swappable>().swapID == swapID && Draggable.justReleased)
                     {
+                        Draggable.justReleased = false;
+
                         Vector3 temp = heldDrag.startPos;
                         heldDrag.startPos = thisDrag.startPos;
                         thisDrag.startPos = temp;
@@ -37,9 +37,15 @@ public class Swappable : MonoBehaviour
                         thisDrag.isGoingBack = true;
                         heldDrag.isGoingBack = true;
                     }
+
+                    else
+                    {
+                        Debug.Log("shake");
+                    }
                 }
+
+
             }
-        }
     }
 
     //this is collision
@@ -64,13 +70,13 @@ public class Swappable : MonoBehaviour
         return false;
     }
 
-    void OnGUI()
-    {
-        RectTransform rectTrans1 = this.GetComponent<RectTransform>();
-        GUI.Box(new Rect(rectTrans1.localPosition.x + 725, -rectTrans1.localPosition.y + 350, rectTrans1.rect.width, rectTrans1.rect.height), this.name);
-        Debug.Log(this.name + " " + rectTrans1.localPosition.y + 350 + " " + rectTrans1.transform.position);
-        GUI.Box(new Rect(0, 0, rectTrans1.rect.width, rectTrans1.rect.height), "ORIGIN POINT");
-        GUI.Box(new Rect(0, 100, rectTrans1.rect.width, rectTrans1.rect.height), "PLUS 100 Y");
-    }
+    //void OnGUI()
+    //{
+    //    RectTransform rectTrans1 = this.GetComponent<RectTransform>();
+    //    GUI.Box(new Rect(rectTrans1.localPosition.x + 725, -rectTrans1.localPosition.y + 350, rectTrans1.rect.width, rectTrans1.rect.height), this.name);
+    //    Debug.Log(this.name + " " + rectTrans1.localPosition.y + 350 + " " + rectTrans1.transform.position);
+    //    GUI.Box(new Rect(0, 0, rectTrans1.rect.width, rectTrans1.rect.height), "ORIGIN POINT");
+    //    GUI.Box(new Rect(0, 100, rectTrans1.rect.width, rectTrans1.rect.height), "PLUS 100 Y");
+    //}
 
 }
