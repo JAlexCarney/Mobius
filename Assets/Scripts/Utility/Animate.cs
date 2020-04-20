23891db10458388bf65f198fb35668187d4834a9
Paula <pteriwinkle@gmail.com>
2020-04-20 14:17:32 -0700

    Play and loop buttons added to the animate component

--- Assets/Scripts/Utility/Animate.cs
+++ Assets/Scripts/Utility/Animate.cs
@@ -7,21 +7,20 @@ public class Animate : MonoBehaviour
 {
 
     //hi if this anim plays in front of something clickable UNCLICK raycast target in image component
-
     public Sprite[] frames;
+    public int framesPerSecond = 30;
+    public bool play = true;
+    public bool loop = true;
+
     private int currentFrame;
     private int lastIndex;
-
-    public int framesPerSecond = 30;
     Image imageComponent;
-    //public bool play; ---> play when the canvas is showing
-    //bool loop = true;
 
     // Start is called before the first frame update
     void Start()
     {
         imageComponent = this.GetComponent<Image>();
-        currentFrame = 0;
+        currentFrame = -1;
         lastIndex = 0;
         imageComponent.sprite = frames[0]; //set image
         imageComponent.color = new Color32(255, 255, 255, 255);
@@ -31,20 +30,41 @@ public class Animate : MonoBehaviour
     //this does NOT work with 1 frame per second !!! aint dealin w that srry
     void Update()
     {
-        float index = Time.time - (int)Time.time; //get partial seconds since last second
-        index = (int)(index * framesPerSecond); //get current frame
-        Debug.Log("current frame: " + currentFrame + " index: " + index + " time: " + (Time.time - (int)Time.time));
-        if (lastIndex != index)
+        
+        if (play)
         {
-            lastIndex = (int)index;
-            currentFrame++;
-            if (currentFrame >= frames.Length)
-                currentFrame = 0;
+               
+            float index = Time.time - (int)Time.time; //get partial seconds since last second
+            index = (int)(index * framesPerSecond); //get current frame
 
-            Debug.Log(currentFrame);
-            imageComponent.sprite = frames[this.currentFrame];
-            imageComponent.color = new Color32(255, 255, 225, 255); //for some reason everything gets set to 0 otherwise
+            //if there is any change in frame index update it!!
+            if (lastIndex != index)
+            {
+                lastIndex = (int)index;
+                currentFrame++;
 
+                if (currentFrame >= frames.Length)
+                {
+                    if (loop) //reset anim
+                    {
+                        currentFrame = 0;
+                    }
+                    else //stop anim & display last frame
+                    {
+                        currentFrame = frames.Length - 1;
+                        play = false;
+                    }
+                }
+
+                imageComponent.sprite = frames[this.currentFrame];
+                imageComponent.color = new Color32(255, 255, 225, 255); //for some reason everything gets set to 0 otherwise
+            }
+        }
+        else if (!loop) //start at beginning
+
+        {
+            currentFrame = -1;
+            lastIndex = 0;
         }
     }
 }
