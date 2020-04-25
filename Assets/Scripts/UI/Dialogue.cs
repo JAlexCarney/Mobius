37694cfbae75227ebfe1a58a844d56eadadce6fe
Paula <pteriwinkle@gmail.com>
2020-04-24 20:21:21 -0700

    Basic Dialogue functionality working, only thing to add is have potential dialogue display on room enter

---
+++ Assets/Scripts/UI/Dialogue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// TO USE: 
+/// attach this to a game object
+/// have its dialogue be a child of the object
+/// thats it lmao
+/// </summary>
+public class Dialogue : MonoBehaviour, IPointerDownHandler
+{
+
+    private GameObject dialogue;
+    private DialogueManager dialogueManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Debug.Log(this.GetComponentInChildren<Text>(true).gameObject);
+        dialogue = this.GetComponentInChildren<Text>(true).gameObject;
+        dialogueManager = GameObject.Find("DialogueManager").GetComponent<DialogueManager>(); 
+    }
+
+    public void OnPointerDown(PointerEventData d)
+    {
+        dialogueManager.displayText(dialogue);
+    }
+
+}
---
+++ Assets/Scripts/UI/DialogueManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    private GameObject currentlyDisplayedText;
+    private bool pause; //to prevent the same click enabling and disabling text in 1 frame
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentlyDisplayedText = null;
+        pause = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pause && Input.GetMouseButtonDown(0) && currentlyDisplayedText != null)
+        {
+            Debug.Log("omfffg");
+            currentlyDisplayedText.SetActive(false);
+            currentlyDisplayedText = null;
+        }
+        pause = false;
+
+    }
+
+    public void displayText(GameObject text)
+    {
+        if (currentlyDisplayedText == null)
+        {
+            Debug.Log("booo");
+            pause = true; 
+            currentlyDisplayedText = text;
+            text.SetActive(true);
+        }
+    }
+}
