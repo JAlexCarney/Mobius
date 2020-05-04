086ee04c8d2f7b230c6afd9d1379991573c0d687
Paula <pteriwinkle@gmail.com>
2020-05-04 14:31:00 -0700

    narrative prefabs created, intro anim created

---
+++ Assets/Scripts/UI/IntroTransition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class IntroTransition : MonoBehaviour, IPointerDownHandler
+{
+    Animator animator;
+    Animator transition;
+    int frameCount;  
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        transition = GameObject.Find("Transition").GetComponent<Animator>();
+        frameCount = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        frameCount++; 
+        if (frameCount == 644) 
+        {
+            transition.enabled = true;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+}
