91b973f4dec0bd25a8d9c49a453d97971bfa4b82
J Alex Carney <JAlexCarney@gmail.com>
2020-05-12 14:25:39 -0700

    Created Start Menu

---
+++ Assets/Scripts/Utility/Options.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Options : MonoBehaviour
+{
+    bool open = false;
+
+    private void Start()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            Destroy(GameObject.Find("QuitToDesktop"));
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Toggle();
+        }
+    }
+
+    public void Toggle()
+    {
+        if (open)
+        {
+            Close();
+        }
+        else
+        {
+            Open();
+        }
+    }
+
+    void Open()
+    {
+        Util.ActivateChildren(this.gameObject);
+        open = true;
+    }
+
+    void Close()
+    {
+        Util.DeactivateChildren(this.gameObject);
+        open = false;
+    }
+
+    public void ExitToDesktop()
+    {
+        Application.Quit();
+    }
+
+    public void Fullscreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("StartMenu", LoadSceneMode.Single);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+    }
+}
