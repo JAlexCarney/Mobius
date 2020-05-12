c44645c7fdedfc42e46947011e497e97b1e2c967
J Alex Carney <JAlexCarney@gmail.com>
2020-05-12 12:34:57 -0700

    Fixing on start bug.

--- Assets/Scripts/Utility/TriggerOnStart.cs
+++ Assets/Scripts/Utility/TriggerOnStart.cs
@@ -13,6 +13,10 @@ public class TriggerOnStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Invoke("TriggerEvent", delay);
+    }
+    void TriggerEvent()
+    {
         eventToTrigger.Invoke();
     }
 
